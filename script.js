82f1292e5c9c9499fe22ba07df3c04fb503ad787
Billan Abdi <billanabdi04@gmail.com>
2023-11-09 02:32:30 +0400

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+  } else if (bill > 300) {
+    tip = bill * 0.2;
+  }
+  console.log(
+    `The bill is ${bill}, the tip is ${tip}, and the total cost is ${
+      bill + tip
+    }`
+  );
+}
+
+console.log(calcTip(78));
